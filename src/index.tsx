f29372a8283a9015dd36821d768419bf52503c59
jaagualimpia <jaagualimpia25@gmail.com>
2023-09-30 01:35:10 -0500

    Creating first routes

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([{
+    path: '/',
+    element: <div><p>Home</p></div>,
+  },
+  {
+    path: '/login',
+    element: <div><p>login</p></div>,
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
